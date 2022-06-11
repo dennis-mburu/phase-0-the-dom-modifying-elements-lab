aa1ca58d97b0895d7bd001c8d8feb34dc8bc14b9
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-11 17:24:16 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+main = document.getElementById('main');
+main.remove()
+
+// The following 2 codes also work for task 1
+// document.getElementById('main').remove();
+// document.querySelector("#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+
+// The nect line can also work for task 2
+// newHeader.setAttribute('id', 'victory')
+
+
+newHeader.textContent = "Dennis Mburu is the champion"
+
+
+document.body.append(newHeader);
+
+
+
